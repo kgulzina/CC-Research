04f20438aa5cd37ff9db3b6eff83ac448e1b7099
kgulzina <gkuttubekova@gmail.com>
2018-11-26 12:54:45 -0600

    Modify some functions (see comments in it)

--- weight_predictor.R
+++ weight_predictor.R
@@ -2,7 +2,7 @@
 # Simulate X = {x_1, x_2, ... , x_n} where each x_i is a vector of length T+1,
 # s.t length(x_i)=T+1. X ~ GP(0, Sigma). Sigma is AR(1) with rho=0.95
 
-library(MASS) #github
+library(MASS)
 library(mvtnorm)
 library(Matrix)
 
@@ -20,8 +20,8 @@ Sigma <- function(t, rho){ # n:sample size; rho is fixed
 
 
 # sample size and Time: T << n
-n <- 100
-Time <- 3
+n <- 50
+Time <- 2
     
 # sigma for MVN simulation: n samples
 sigma <- Sigma(Time+1, rho = 0.95)
@@ -63,7 +63,7 @@ dist_mtx <- calc_dist(x_values, Time, n)
 
 
 
-# a correlation matrix: (using corr(y_x_i, y_x_j))
+# a correlation matrix: (using corr(y_x_i, y_x_j)) -- For simulation
 calc_corr <- function(val, t, n) { #simulated X's
     omega <- matrix(NA, nrow = n, ncol = n)
     time <- 0:t
@@ -200,12 +200,13 @@ find_sigma <- function(rho, t){
     time <- seq(0, t, by = 1)
     v <- (t-time+1)/(t+1)
     for(i in 1:(length(v)-1)) {
-        delta <- v[i+1]-rho*v[i]
+        z[i] <- v[i+1]-rho*v[i]
     }
-    return(sd(v))
+    return(var(z))
 }
 
-find_sigma(0.8, Time)
+
+find_sigma(0.8, Time) ### totally wrong!
 
 # new log_lkl w/ penalty term to optimize:
 loglkl_mvn_penalty <- function(w,d) {
@@ -219,7 +220,7 @@ loglkl_mvn_penalty <- function(w,d) {
             ss <- 0
             for(t in 1:time){
                 #restriction on w(t)
-                w[t+1] <- 0.8*w[t] + rnorm(1, 0, s)
+                w[t+1] <- 0.8*w[t] + rnorm(1, w[t], .001)
                 ss <- ss + w[t]*(d[i,t]-d[j,t])^2
             }
             sigma[i,j] <- exp(-ss)
@@ -243,7 +244,7 @@ w_estimate <- function(Time, n, opt_f) {
     sigma <- Sigma(Time+1, rho = 0.95)
     
     # simulate
-    x_values <- mvrnorm(n, rep(0, Time+1), sigma)
+    x_values <- rmvnorm(n, mean = rep(0, Time+1), sigma = sigma)
     
     #calculate omega for Y
     omega <- calc_corr(x_values, Time, n)
@@ -257,22 +258,145 @@ w_estimate <- function(Time, n, opt_f) {
     # set initial values for the parameters
     pars <- seq(1, 1/(Time+1), len = Time+1)
     
+    # find gradient
+    #gr <- calc_gradient(opt_f, 10^-10, d)
+    
     # optimize
     opt <- optim(par = pars, opt_f, d = d, control = list(fnscale = -1,
-                                                          maxit=1000,
-                                                          trace=1))
+                maxit=1000, trace=1), method = "L-BFGS-B",
+                gr = calc_gradient, lower = rep(-1, Time+1), 
+                upper = rep(2, Time+1))
     
-    print(opt$convergence)
     print(pars)
     return(opt)
 }
 
-w_estimate(10, 100, loglkl_mvn_penalty)
+w_estimate(3, 100, loglkl_mvn_penalty)
+
+
+
+#11/05/18
+### commments:
+# "BFGS" gives an error of singularity for SVD: remedy?!
+# "CG" did not work at all: no need for remedy
+# "L-BFGS-B": converges but not reasonable estimates: remedy?!
+# "SANN": coonverges but not reasonable estimates: remedy?!
+# "Nelder-Mead" is giving error 10: degeneracy: remedy????
+
+## now our goal is to find that remedy: calculate gradient
+calc_gradient <- function(w,d){
+    gr <- c()
+    for(i in 1:length(w)) {
+        gr[i] <- (loglkl_mvn_penalty(w[i]+10^(-6),d)-
+                      loglkl_mvn_penalty(w,d))/(10^(-6))
+    }
+    return(gr)
+}
+
+calc_gradient(w, d) #to check if it works
+
+### comments:
+# gradient term should be used with "L-BFGS-B", "BFGS" or "GC"
+# may not converge if X contains missing or infinite values. Most probably
+# infinite values. We can take this into guarantee, assuming that inputs
+# will not contain any infinite values and will not have missing values.
 
 
 ### sketch the graph of a lkl function with penalty term: 
 # general p-dimensional code 
 # keeps selected axes constant 
+library(plotly)
+library(lattice)
+
+# find values of lkl function:
+lkl_values <- function(d){
+    seq <- seq(0, 1, 0.01)
+    z <- matrix(NA, length(seq), length(seq))
+    for(i in 1:length(seq)) {
+        for(j in 1:length(seq)) {
+            z[j,i] <- loglkl_mvn_penalty(c(seq[i], seq[j]), d)
+        }
+    }
+    return(z) # z should be matrix for persp()
+}
+
+lkl_values(d)
+
+
+# special lkl function for plotting:
+loglkl_mvn_penalty <- function(w1,w2,d) {
+    n <- nrow(d)
+    time <- ncol(d)-1
+    sigma <- matrix(NA, ncol = n, nrow = n)
+    s <- find_sigma(0.8, time-1)
+    
+    for(i in 1:n){
+        for(j in 1:n){
+                ss <- w1*(d[i,1]-d[j,1])^2 + w2*(d[i,2]-d[j,2])^2
+            }
+            sigma[i,j] <- exp(-ss)
+        }
+    
+    result <- -(1/2)*(n*log(2*pi)+determinant(sigma, logarithm = TRUE)$modulus) + 
+        (t(d[,t+1])%*%ginv(sigma)%*%d[,t+1])/2
+    # used ginv()
+}
+
+
+# plot w/out color
+lkl_plot <- function(Time,n) {
+    sigma <- Sigma(Time+1, rho = 0.95)
+    
+    # simulate
+    x_values <- mvrnorm(n, rep(0, Time+1), sigma)
+    
+    #calculate omega for Y
+    omega <- calc_corr(x_values, Time, n)
+    
+    # simulate
+    y <- rmvnorm(1, mean = rep(0, n), sigma = omega)
+    
+    # combine datasets: functional input and scale output
+    d <- cbind(x_values,t(y))
+    
+    # keep one axis constant, calculate z from outside
+    z <- lkl_values(d)
+    
+    persp(seq(0, 1, 0.01), seq(0, 1, 0.01), z, phi = 45, theta = 0,
+          xlab = "weight at time i", ylab = "weight at time j",
+          main = "Log-likelihood with penalty", zlim = range(z, na.rm = 0)
+          # plot -z, since lkl is negative
+    )
+}
+
+lkl_plot(1,50) # non-differentiable surface (very bad)!!!!!!
+
+
+# plot w/ color
+lkl_plot_col <- function(Time,n) {
+    sigma <- Sigma(Time+1, rho = 0.95)
+    
+    # simulate
+    x_values <- mvrnorm(n, rep(0, Time+1), sigma)
+    
+    #calculate omega for Y
+    omega <- calc_corr(x_values, Time, n)
+    
+    # simulate
+    y <- rmvnorm(1, mean = rep(0, n), sigma = omega)
+    
+    # combine datasets: functional input and scale output
+    d <- cbind(x_values,t(y))
+    
+    # keep one axis constant, calculate z from outside
+    z <- lkl_values(d)
+    
+    wireframe(seq(0, 1, 0.01), seq(0, 1, 0.01), z, phi = 45, theta = 45,
+          xlab = "weight at time i", ylab = "weight at time j",
+          main = "Log-likelihood with penalty", colorkey = TRUE)
+}
+
+lkl_plot_col(1,50)
 
 
 
