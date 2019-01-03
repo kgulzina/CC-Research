0c20d55fd79d28c030b145a1aecfa95fdda0c4fb
kgulzina <gkuttubekova@gmail.com>
2019-01-03 15:47:45 -0600

    Created separate R file for the simulation

---
+++ test_on_simulation.R
@@ -0,0 +1,79 @@
+####### This code tests "loglkl_with_penalty" with the simulated data #######
+## log is given "README.md" file in github
+## Author: stat_cat
+## Date: 09/03/18 - present
+
+
+library(MASS)
+library(mvtnorm)
+library(Matrix)
+library(tmvtnorm)
+
+
+## conventions: 
+## sample size and Time: T << n
+n <- 100
+Time <- 3 # if 1 -- cannot calculate the sigmasq
+rho <- 0.99
+sigmasq <- 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
