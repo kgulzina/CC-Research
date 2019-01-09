2d334e3adf81d16edea95483ee2f739e82916fd8
kgulzina <gkuttubekova@gmail.com>
2019-01-09 15:33:38 -0600

    Check for the accuracy of simulated data has been added

--- test_on_simulation.R
+++ test_on_simulation.R
@@ -21,10 +21,45 @@ sigmasq <- 1
 source('supplementary_functions.R')
 
 ## simulate weights:
-w <- simulate_w
+w <- simulate_w_trnctd(Time)
 
-simulate_d(Time, n, )
+## simulate data:
+d <- simulate_d(Time, n, w)
 
+
+
+#### Some manipulations to see if Y's are sampled correctly ####
+## summary statistics and simple plot
+y <- d[,ncol(d)]
+summary(y)
+plot(y)
+
+## color map:
+# we need positive y_values:
+positive_y <- y + abs(min(y)) #shift by the minimum value
+plot(positive_y)
+min1_y <- positive_y + (1-min(positive_y)) #set minimum value of y = 1
+plot(min1_y)
+
+# convert to colors:
+colors <- colorRampPalette(c("red", "orange", "blue"))(3)[min1_y]
+ 
+# check if all Y's have been assigned colors: n is good
+length(colors)
+
+# add to dataframe:
+to_check <- as.data.frame(d[,c(1,2)])
+colnames(to_check) <- c("x0","x1")
+to_check$colors <- colors
+
+# plotted are X's (default: n=100) and points are colored by Y:
+plot(to_check$x0, to_check$x1, col = to_check$colors)
+
+
+##### close dots (dy distance) should be of the same color #####
+
+
+## 
 
 
 
