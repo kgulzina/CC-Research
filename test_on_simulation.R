f6802669dc1b2bbb2ee8000a93b6defc86a567e3
kgulzina <gkuttubekova@gmail.com>
2019-01-10 01:11:28 -0600

    Included 3D plots for estimation

--- test_on_simulation.R
+++ test_on_simulation.R
@@ -8,12 +8,14 @@ library(MASS)
 library(mvtnorm)
 library(Matrix)
 library(tmvtnorm)
+library(plotly)
+library(lattice)
 
 
 ## conventions: 
 ## sample size and Time: T << n
 n <- 100
-Time <- 10 
+Time <- 1 
 rho <- 0.99
 sigmasq <- 1
 
@@ -21,12 +23,15 @@ sigmasq <- 1
 source('supplementary_functions.R')
 source('loglkl_with_penalty.R')
 source("assess_accuracy.R")
+source("3D_plot_loglkl.R")
+
 
 ## simulate weights:
 w <- simulate_w_trnctd(Time)
 
 ## simulate data:
 d <- simulate_d(Time, n, w)
+
 
 
 
@@ -57,12 +62,12 @@ to_check$colors <- colors
 # plotted are X's (default: n=100) and points are colored by Y:
 plot(to_check$x0, to_check$x1, col = to_check$colors)
 
-
 ##### close dots (dy distance) should be of the same color #####
 
 
 
-## initial values:
+
+## set initial values:
 pars <- seq(1, 1/(Time+1), len = Time+1)
 pars
 
@@ -73,16 +78,21 @@ est2 <- estimate_w(loglkl_mvn_penalty, calc_gradient_num, w, d, 10000)
 # difference in estimates:
 diff_btw_estimates(est1$par, est2$par)
 
-# plot
+# plot of differences:
 plot_residuals(est1$par, est2$par)
+
+
 
 
 ## NEW:
 ## Concern: there is small discrepancy in estimates, so optim() might be 
 ## converging to local maxima. To find out, we get 3D plot:
 
+# plot without color
+lkl_plot(d) 
 
-
+# plot w/ color
+lkl_plot_col(d)
 
 
 
