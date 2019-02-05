47d914d3e8af124103214f3ac675ae1cf5849351
kgulzina <gkuttubekova@gmail.com>
2019-02-05 01:54:53 -0600

    Created new file on WEPP dataset

---
+++ wepp_data.R
@@ -0,0 +1,22 @@
+######### This code contains WEPP data related information #########
+## Author: stat_cat
+## Date: 02/04/19
+
+
+
+# STRIPS-1 Configurations -------------------------------------------------
+library("devtools")
+install_github("ISU-STRIPS/STRIPS")
+STRIPSMeta::watersheds
+
+# comments: meta data is not informative at all
+
+
+
+# Miller’s paper ----------------------------------------------------------
+
+
+
+
+
+
